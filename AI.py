381de7fa9dc0e7881692b162220347ebebe8bf2b
Nidal El Fengaoui <nidal.fengaoui@gmail.com>
2020-06-12 23:39:52 +0200

    Finally it working

--- AI.py
+++ AI.py
@@ -2,6 +2,7 @@ from joueurlib import IA
 
 from interfacelib import couleur_to_couleurval as Cl_val
 import numpy as np
+from time import time as getTime
 
 class Random(IA):
 	def demande_coup(self):
@@ -15,47 +16,131 @@ class Random(IA):
 
 class MinMax(IA):
 
-	def getValue(self):
-		score = self.jeu.plateau.score()
-		B_Score, W_Score = score[1], score[2]
-		return B_Score - W_Score if self.couleur == "noir" else W_Score - B_Score
+	Nbr_Simulated_Moves = 0
 
 	def demande_coup(self):
+		Start = getTime() 
+		self.Nbr_Simulated_Moves = 0
 		Possible_Moves = self.jeu.plateau.liste_coups_valides(Cl_val(self.couleur))
+		
 		if len(Possible_Moves) == 0 :
 			return []
 
 		BestScore = -float("inf")
 		Chosen_Move = []
-
+		print(self.couleur, " : ")
 		for Move in Possible_Moves :
 			Tmp_Board = self.jeu.plateau.copie()
 			Tmp_Board.jouer(Move, Cl_val(self.couleur))
-			Score = self.min_max(Tmp_Board, 3, False)
+			self.Nbr_Simulated_Moves += 1
+			Score = self.min_max(Tmp_Board, 3, False, Cl_val(self.couleur))
+			print("\t", Move, " ==> ", Score)
 			if BestScore <= Score :
 				BestScore = Score
 				Chosen_Move = Move
+		End = getTime()
+		print("\n\tChosen Move ==> ", Chosen_Move, " after ", self.Nbr_Simulated_Moves, " simulations (",End-Start," s )")
 		return Chosen_Move
-
-	def min_max(self, Board, Depth, onMaximizing):		
-
-		Possible_Moves = Board.liste_coups_valides(Cl_val(self.couleur))		
-		if Depth == 0 or len(Possible_Moves) == 0:
-			return self.getValue()
+	
+	def min_max(self, Board, Depth, onMaximizing, Color):		
+		if Depth == 0 :
+			return getValue(Board, Color)
 
 		BestScore = -float("inf") if onMaximizing else float("inf")
 
 		if onMaximizing :
+			Possible_Moves = Board.liste_coups_valides(Color)
 			for Move in Possible_Moves :
 				Tmp_Board = Board.copie()
-				Tmp_Board.jouer(Move, Cl_val(self.couleur))
-				Score = self.min_max(Tmp_Board, Depth - 1, not onMaximizing)
+				Tmp_Board.jouer(Move, Color)
+				self.Nbr_Simulated_Moves += 1
+				Score = self.min_max(Tmp_Board, Depth - 1, not onMaximizing, -Color)
 				BestScore = max(Score, BestScore)
 			return BestScore
+
 		else :	
+			Possible_Moves = Board.liste_coups_valides(-Color)
 			for Move in Possible_Moves :
 				Tmp_Board = Board.copie()
-				Tmp_Board.jouer(Move, Cl_val(self.couleur))
-				Score = self.min_max(Tmp_Board, Depth - 1, onMaximizing)
+				Tmp_Board.jouer(Move, -Color)
+				self.Nbr_Simulated_Moves += 1
+				Score = self.min_max(Tmp_Board, Depth - 1, not onMaximizing, -Color)
 				BestScore = min(Score, BestScore)
 			return BestScore
+
+class AlphaBeta(IA):
+	
+	def demande_coup(self):
+		Start = getTime() 
+		self.Nbr_Simulated_Moves = 0
+		Possible_Moves = self.jeu.plateau.liste_coups_valides(Cl_val(self.couleur))
+		
+		if len(Possible_Moves) == 0 :
+			return []
+
+		BestScore = -float("inf")
+		Chosen_Move = []
+		print(self.couleur, " : ")
+		for Move in Possible_Moves :
+			Tmp_Board = self.jeu.plateau.copie()
+			Tmp_Board.jouer(Move, Cl_val(self.couleur))
+			self.Nbr_Simulated_Moves += 1
+			Score = self.alpha_beta(Tmp_Board, 3, False, Cl_val(self.couleur))
+			print("\t", Move, " ==> ", Score)
+			if BestScore <= Score :
+				BestScore = Score
+				Chosen_Move = Move
+		End = getTime()
+		print("\n\tChosen Move ==> ", Chosen_Move, " after ", self.Nbr_Simulated_Moves, " simulations (",End-Start," s )")
+		return Chosen_Move
+
+	def alpha_beta(self, Board, Depth, onMaximizing, Color, Alpha=-float("inf"), Beta=float("inf")):		
+	
+		if Depth == 0 :
+			return getValue(Board, Color)
+
+		BestScore = -float("inf") if onMaximizing else float("inf")
+
+		if onMaximizing :
+			Possible_Moves = Board.liste_coups_valides(Color)
+			for Move in Possible_Moves :
+				Tmp_Board = Board.copie()
+				Tmp_Board.jouer(Move, Color)
+				self.Nbr_Simulated_Moves += 1
+				Score = self.alpha_beta(Tmp_Board, Depth - 1, not onMaximizing, -Color)
+				BestScore = max(Score, BestScore)
+				Alpha = max(Alpha,BestScore)
+				if Alpha >= Beta :
+					break
+			return BestScore
+
+		else :	
+			Possible_Moves = Board.liste_coups_valides(-Color)
+			for Move in Possible_Moves :
+				Tmp_Board = Board.copie()
+				Tmp_Board.jouer(Move, -Color)
+				self.Nbr_Simulated_Moves += 1
+				Score = self.alpha_beta(Tmp_Board, Depth - 1, not onMaximizing, -Color)
+				BestScore = min(Score, BestScore)
+				Beta = min(Beta, BestScore)
+				if Beta <= Alpha :
+					break
+			return BestScore		
+
+def getValue(Board, Color):
+	My_Score, Enm_Score = 0, 0
+	for i in range(8):
+		for j in range(8):
+			if Board.tableau_cases[i][j] == Color:
+				if i in [0,7] and j in [0, 7]:
+					if i == j or [i, j] == [0, 7] or [i, j] == [7, 0]: 
+						My_Score += 5
+					My_Score += 5
+				My_Score += 1
+			elif Board.tableau_cases[i][j] == -Color:
+				if i in [0,7] and j in [0, 7]:
+					if i == j or [i, j] == [0, 7] or [i, j] == [7, 0]: 
+						Enm_Score += 5
+					Enm_Score += 5
+				Enm_Score += 1
+	return My_Score - Enm_Score
--- Test.py
+++ Test.py
@@ -6,7 +6,11 @@ import numpy as np
 Ran = AI.Random
 Hum = joueurlib.Humain
 MM  = AI.MinMax
+AB  = AI.AlphaBeta
 
 J = Jeu(opts={"choix_joueurs" : {"noir":MM, "blanc":MM}})
-#J = Jeu()
+# J = Jeu()
 J.demarrer()
+
+# J2 = Jeu(opts={"choix_joueurs" : {"noir":AB, "blanc":AB}})
+# J2.demarrer()
